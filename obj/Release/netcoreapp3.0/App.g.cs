60da8528812ce48722b0b60954ed897171ea001c
Nico Brownell <nmbrownell1@gmail.com>
2023-01-20 12:09:00 -0600

    Post day 3 commit

--- obj/Release/netcoreapp3.0/App.g.cs
+++ obj/Release/netcoreapp3.0/App.g.cs
@@ -45,7 +45,7 @@ namespace The_Oracle {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace The_Oracle {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             The_Oracle.App app = new The_Oracle.App();
             app.InitializeComponent();
